7740704e9486c07ccc619e2d7a505d2c0ce5461d
entgondoru <entgondoru@wp.pl>
2019-09-17 13:02:20 +0200

    asd

---
+++ PAI/poczatek/zmienne.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+     $name = 'janusz';
+     $_name1 = 'janusz';
+     $name = 'janusz';
+     ?>
+  </body>
+</html>
